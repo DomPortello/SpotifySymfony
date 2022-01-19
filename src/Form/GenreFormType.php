1577a7d46009d86e3b485908976e182266b3a2ad
Nico <nicolas.flak@gmail.com>
2022-01-19 14:39:11 +0100

    build form

--- src/Form/GenreFormType.php
+++ src/Form/GenreFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Genre;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,12 +13,42 @@ class GenreFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('smallPicture')
-            ->add('mediumPicture')
-            ->add('bigPicture')
-            ->add('xlPicture')
-            ->add('albums')
+            ->add('name', TextType::class, [
+                'label' => 'Genre',
+                'attr' => [
+                    'placeholder' => 'Genre'
+                ]
+            ])
+            ->add('smallPicture', TextType::class, [
+                'label' => 'Url petite image',
+                'attr' => [
+                    'placeholder' => 'Url petite image'
+                ]
+            ])
+            ->add('mediumPicture', TextType::class, [
+                'label' => 'Url image moyenne',
+                'attr' => [
+                    'placeholder' => 'Url image moyenne'
+                ]
+            ])
+            ->add('bigPicture', TextType::class, [
+                'label' => 'Url grande image',
+                'attr' => [
+                    'placeholder' => 'Url grande image'
+                ]
+            ])
+            ->add('xlPicture', TextType::class, [
+                'label' => 'Url image XL',
+                'attr' => [
+                    'placeholder' => 'Url image XL'
+                ]
+            ])
+            ->add('albums', TextType::class, [
+                'label' => "Nom de l'album",
+                'attr' => [
+                    'placeholder' => "Nom de l'album"
+                ]
+            ])
         ;
     }
 
